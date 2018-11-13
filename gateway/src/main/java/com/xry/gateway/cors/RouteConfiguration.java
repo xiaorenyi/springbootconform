723e5471d9488890be085838549f3b87d59a1d62
yuqinggen <15622329294@163.com>
2018-11-13 21:30:15 +0800

    1、aop permission实现

--- gateway/src/main/java/com/xry/gateway/cors/RouteConfiguration.java
+++ gateway/src/main/java/com/xry/gateway/cors/RouteConfiguration.java
@@ -28,7 +28,7 @@ public class RouteConfiguration {
     private static final String ALLOWED_HEADERS = "*";
     private static final String ALLOWED_METHODS = "*";
     private static final String ALLOWED_ORIGIN = "*";
-    private static final String ALLOWED_Expose = "*";
+    private static final String ALLOWED_EXPOSE = "*";
     private static final String MAX_AGE = "18000L";
 
     @Bean
@@ -45,7 +45,7 @@ public class RouteConfiguration {
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
                 headers.add("Access-Control-Max-Age", MAX_AGE);
                 headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-                headers.add("Access-Control-Expose-Headers", ALLOWED_Expose);
+                headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSE);
                 headers.add("Access-Control-Allow-Credentials", "true");
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);
